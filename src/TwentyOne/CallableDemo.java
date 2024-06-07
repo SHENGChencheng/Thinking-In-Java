aed631711a1d7476d66509b72371746a3b0f1d0a
SHENG_Chencheng <shengchencheng@foxmail.com>
2024-06-08 04:51:37 +0800

    2024/6/8

---
+++ src/TwentyOne/CallableDemo.java
@@ -0,0 +1,37 @@
+package TwentyOne;
+import java.util.concurrent.*;
+import java.util.*;
+
+class TaskWithResult implements Callable<String> {
+    private int id;
+    public TaskWithResult(int id) {
+        this.id = id;
+    }
+    public String call() {
+        return "result of TaskWithResult " + id;
+    }
+}
+
+public class CallableDemo {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        ArrayList<Future<String>> results = new ArrayList<Future<String>>();
+        for (int i = 0; i < 10; i++) {
+            TaskWithResult task = new TaskWithResult(i);
+            Future<String> result = exec.submit(task);
+            results.add(result);
+        }
+        for (Future<String> fs : results) {
+            try {
+                System.out.println(fs.get());
+            } catch (InterruptedException e) {
+                System.out.println(e);
+                return;
+            } catch (ExecutionException e) {
+                System.out.println(e);
+            } finally {
+                exec.shutdown();
+            }
+        }
+    }
+}
